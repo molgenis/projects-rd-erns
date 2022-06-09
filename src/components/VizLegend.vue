a649508986e5876b399be845abf5fe4a127dcdbc
davidruvolo51 <davidruvolo51@gmail.com>
2022-06-09 16:30:42 +0200

    init legend component

---
+++ src/components/VizLegend.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="legend">
+    <li class="legend-item" data-legend-item="item" v-for="(label, index) in labels" :key="label">
+      <svg class="item-marker" width="16" height="16" viewBox="0 0 16 16" preserveXminYmin="true">
+        <circle cx="8" cy="8" r="8" :fill="colors[index]" stroke-width="none"></circle>
+      </svg>
+      <span class="item-label">{{ label }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    labels: {
+      type: Array,
+      required: true
+    },
+    colors: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.legend {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  
+  .item-marker {
+    margin-right: 4px;
+  }
+  
+  .item-label {
+    line-height: 1.4;
+  }
+  
+}
+</style>
