02f2c2ec2fa3bc12d04d76738aedd15a7b402dad
davidruvolo51 <davidruvolo51@gmail.com>
2022-10-13 09:23:55 +0200

    added transparency colors

--- app/src/components/RouterLinkCard.vue
+++ app/src/components/RouterLinkCard.vue
@@ -61,7 +61,7 @@ export default {
       left: 0;
       width: 100%;
       height: 100%;
-      background-color: $black-transparent-100;
+      background-color: $gray-transparent-100;
     }
   }
 }
