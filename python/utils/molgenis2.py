8ace5097094725a010f0d7125d58f0b0e80387c8
davidruvolo51 <davidruvolo51@gmail.com>
2022-12-06 10:32:55 +0100

    added file import method

--- python/utils/molgenis2.py
+++ python/utils/molgenis2.py
@@ -10,11 +10,13 @@
 #'////////////////////////////////////////////////////////////////////////////
 
 import molgenis.client as molgenis
-from os.path import abspath
-import numpy as np
 from datetime import datetime
+from os import path
+import numpy as np
+import mimetypes
 import tempfile
 import pytz
+import json
 import csv
 
 class Molgenis(molgenis.Session):
@@ -55,7 +57,7 @@ class Molgenis(molgenis.Session):
     with tempfile.TemporaryDirectory() as tmpdir:
       filepath=f"{tmpdir}/{pkg_entity}.csv"
       self._datatableToCsv(filepath, data)
-      with open(abspath(filepath),'r') as file:
+      with open(path.abspath(filepath),'r') as file:
         response = self._session.post(
           url = self.fileImportEndpoint,
           headers = self._headers.token_header,
@@ -68,3 +70,51 @@ class Molgenis(molgenis.Session):
           self._print('Imported data into', pkg_entity)
         return response
 
+  def importFile(self, file):
+    """Import a file into Molgenis
+    Import a file (pdf, txt, docx, etc.) into the files table. Content type
+    and size is automatically determined.
+    
+    @param file location and name of the file to import
+    
+    @section
+    
+    If the file was successfully imported, you will receive information about
+    the file and the location in the database. Use the file identifier to set
+    additional permissions. This can be done using the molgenis commander.
+    
+    ```zsh
+    mcmd config set host
+    mcmd give \ 
+      --user <user> <read|view|write|...> \
+      --entity <file-identifier-in-database> \
+      --package sys_FileMetaa
+    ```
+    
+    @return a status message with import metadata
+    """
+    with open(file, 'rb') as stream:
+      data = stream.read()
+      stream.close()
+    
+    headers = self._headers.token_header
+    headers['x-molgenis-filename'] = file
+    headers['Content-Type'] = mimetypes.guess_type(file)[0]
+    headers['Content-Size'] = str(path.getsize(file))
+    
+    url = f"{self._root_url}api/files"
+    response = self._session.post(
+      url = url,
+      headers = headers,
+      data = data
+    )
+
+    if response.status_code // 100 == 2:
+      result = json.loads(response.text)
+      print(response.status_code)
+      print(result)
+      return response
+    else:
+      print(f"Failed to import {file}")
+      print(response.status_code)
+      return response
