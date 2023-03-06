cf1231606fdae11b5c928b7a1972e207c18c1973
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-06 15:11:46 +0100

    initial working script

--- erns/genturis/data_stats_summarise.py
+++ erns/genturis/data_stats_summarise.py
@@ -2,14 +2,14 @@
 # FILE: data_stats_summarise.py
 # AUTHOR: David Ruvolo
 # CREATED: 2023-02-16
-# MODIFIED: 2023-02-21
+# MODIFIED: 2023-03-06
 # PURPOSE: summarise data for dashboard
-# STATUS: in.progress
+# STATUS: stable
 # PACKAGES: **see below**
 # COMMENTS: NA
 #///////////////////////////////////////////////////////////////////////////////
 
-from python.utils.molgenis2 import Molgenis
+from erns.utils.molgenis2 import Molgenis
 from datatable import dt, f, as_type
 from dotenv import load_dotenv
 from datetime import datetime
@@ -32,7 +32,6 @@ def print2(*args):
   message = ' '.join(map(str, args))
   time = datetime.now(tz=pytz.timezone('Europe/Amsterdam')).strftime('%H:%M:%S.%f')[:-3]
   print(f'[{time}] {message}')
-
 
 def flattenDataset(data, columnPatterns=None):
   """Flatten Dataset
@@ -95,7 +94,60 @@ def yearToDate(value, format="-07-01"):
   @return a date string in yyyy-mm-dd format
   """
   return f"{value}{format}"
+
+def extractColumnNames(data, pattern):
+  cols = data[:, [name for name in data.names if re.search(pattern, name)]].names
+  return list(cols)
   
+def calcRowSums(data, colname, genes, geneColumns, classColumns, zygosityColumns=None):
+  """Calculate Row Sums
+  Calculate row sums based on the count of each gene in genes multiplied by
+  classification of pathogenic or likely pathogenic.
+
+  @param data datatable object
+  @param colname name of the column to create
+  @param genes a list of genes to check
+  @param geneColumns a list of one or more gene columns to check
+  @param classColumns a list of one or more classification columns to check
+  @param zygosityColumns a list of one or more zygosity columns to check
+  
+  @return integer
+  """
+  subjectIDs = data['ID_Patient'].to_list()[0]
+  for id in subjectIDs:
+    row = data[f.ID_Patient == id, :]
+    rowSum = 0
+    for gene in genes:
+      geneCount = 0
+      likelyPathogenicCount = 0
+      pathogenicCount = 0
+      zygosityCount = 0
+
+      for geneColumn in geneColumns:
+        if row[geneColumn].to_list()[0][0] == gene:
+          geneCount += 1 # how many cases of gene X are there?
+
+      for classColumn in classColumns:
+        if row[classColumn].to_list()[0][0] == '2': # likely pathogenic (2)
+          likelyPathogenicCount += 1
+        if row[classColumn].to_list()[0][0] == '3': # pathogenic (3)
+          pathogenicCount += 1
+
+      genePathogenic = geneCount * pathogenicCount
+      geneLikelyPathogenic = geneCount * likelyPathogenicCount
+
+      if zygosityColumns:
+        for zygosityColumn in zygosityColumns:
+          if row[zygosityColumn].to_list()[0][0] == '1': # homozygous germline variant (1)
+            zygosityCount += 1
+
+        genePathZyg = genePathogenic * zygosityCount
+        geneLikelyPathZyg = geneLikelyPathogenic * zygosityCount        
+        rowSum += (genePathZyg + geneLikelyPathZyg)
+      else:
+        rowSum += (genePathogenic + geneLikelyPathogenic)
+    data[f.ID_Patient==id, colname] = rowSum
+
 #///////////////////////////////////////////////////////////////////////////////
 
 # ~ 1 ~
@@ -191,6 +243,10 @@ columns=','.join([
   'VariantGene_2',
   'VariantGene_3',
   'VariantGene_4',
+  'VariantZygosity_1',
+  'VariantZygosity_2',
+  'VariantZygosity_3',
+  'VariantZygosity_4',
   'ORDO',
   'InclCriteriaUnexplained',
 ])
@@ -214,7 +270,7 @@ print2(f"Retrieved {len(subjects)} subjects....")
 # Reshape subjects dataset
 print2('Transforming subjects dataset....')
 
-subjectsDT = flattenDataset(subjects, columnPatterns='id|code')
+subjectsDT = flattenDataset(subjects, columnPatterns='id|code|value')
 subjectsDT = dt.Frame(subjectsDT)
 
 # fix extra white space in subjectID
@@ -222,7 +278,6 @@ subjectsDT['ID_Patient'] = dt.Frame([
   value.strip()
   for value in subjectsDT['ID_Patient'].to_list()[0]
 ])
-
 
 # format YearBirth
 print2('Applying default date format...')
@@ -241,7 +296,6 @@ if 'YearFollowup' in subjectsDT.names:
 else:
   subjectsDT['YearFollowup'] = None
 
-
 # Format YearDeath if available
 if 'YearDeath' in subjectsDT.names:
   subjectsDT['YearDeath'] = dt.Frame([
@@ -251,8 +305,8 @@ if 'YearDeath' in subjectsDT.names:
 else:
   subjectsDT['YearDeath'] = None
 
-
 # find most recent date
+print2('Determining the most recent date....')
 recentDates = subjectsDT[:, (f.DateLastInfo,f.YearFollowup, f.YearDeath)] \
   .to_pandas() \
   .replace({np.nan: None}) \
@@ -263,19 +317,15 @@ subjectsDT['recentDate'] = dt.Frame([
   for row in recentDates
 ])
 
-
 # change class to date32
-subjectsDT[
-  :, dt.update(
-    YearBirth = as_type(f.YearBirth, dt.Type.date32),
-    recentDate = as_type(f.recentDate, dt.Type.date32)
-  )
-]
+subjectsDT[:, dt.update(
+  YearBirth = as_type(f.YearBirth, dt.Type.date32),
+  recentDate = as_type(f.recentDate, dt.Type.date32)
+)]
 
 # format ORDO string
 subjectsDT['ORDO'] = dt.Frame([
-  value.strip() if value else value
-  for value in subjectsDT['ORDO'].to_list()[0]
+  value.strip() if value else value for value in subjectsDT['ORDO'].to_list()[0]
 ])
 
 # check IDs
@@ -286,6 +336,7 @@ if dt.unique(subjectsDT['ID_Patient']).nrows != subjectsDT.nrows:
 
 # ~ 2 ~
 # Summarise data
+print2('Creating summary stats for data-highlights.....')
 
 # ~ 2a ~
 # Calcuate data-highlights
@@ -296,7 +347,9 @@ totalPatients = dt.unique(subjectsDT['ID_Patient']).nrows
 # find active centers to get countries
 activeCenterIDs = dt.unique(subjectsDT['databaseID']).to_list()[0]
 activeCenters = providers[
-  functools.reduce(operator.or_, (f.databaseID == id for id in activeCenterIDs)), :
+  functools.reduce(
+    operator.or_, (f.databaseID == id for id in activeCenterIDs)
+  ), :
 ]
 
 totalCenters = dt.unique(activeCenters['projectName']).nrows
@@ -315,6 +368,7 @@ ernstats[f.id=='data-highlight-2','value'] = totalCenters
 # all row-level data, calculate the age using the most recent date, and then
 # apply bins (as defined in the dashboard). From there, summarise the data
 # by age group
+print2('Calculating age....')
 
 # Calcuate age at the row-level
 subjectsDT['age'] = dt.Frame([
@@ -324,6 +378,7 @@ subjectsDT['age'] = dt.Frame([
 ])
 
 # apply maximum age to missing values
+print2('Applying max.age where age cannot be calculated....')
 if subjectsDT[f.age==None,'age'].nrows > 0:
   print2('Applying max age to missing values...')
   maxAge = subjectsDT[:, dt.max(f.age)].to_list()[0][0]
@@ -333,6 +388,7 @@ if subjectsDT[f.age==None,'age'].nrows > 0:
   ])
 
 # bin age
+print2('Creating bins for age....')
 subjectsPD = subjectsDT.to_pandas()
 bins = [0] + [num for num in range(20,80,10)] + [np.inf]
 labels=['<20','20-29','30-39','40-49','50-59','60-69','70+']
@@ -341,6 +397,7 @@ subjectsPD['bin']=pd.cut(subjectsPD['age'],bins=bins,labels=labels,right=False)
 subjectsDT = dt.Frame(subjectsPD)
 
 # Summarise data and update stats dataset
+print2('Summarising by age range and updating dataset....')
 ageByGroup = subjectsDT[:, dt.count(), dt.by(f.bin)]
 
 for value in ageByGroup['bin'].to_list()[0]:
@@ -355,7 +412,7 @@ for value in ageByGroup['bin'].to_list()[0]:
 
 # ~ 2c ~
 # Summarise sexAtBirth
-
+print2('Summarising sexAtBirth....')
 sexAtBirth = subjectsDT[:, dt.count(), dt.by(f.Sex)]
 sexAtBirth.names = { 'Sex': 'id' }
 
@@ -374,57 +431,59 @@ for value in sexAtBirth['label'].to_list()[0]:
 #///////////////////////////////////////
 
 # ~ 2d ~
-# Apply thematic disease groups (TDG) and summarise
-# 1 = nf
-# 2 = lynch
-# 3 = hboc
-# 4 = other
-
+# Apply thematic disease groups (TDG) and summarise. Each check will return the
+# TDG identifier which will be recoded into group names before merging with the
+# main summary stats dataset. For reference, the group IDs are listed below.
+#
+#  1 = nf
+#  2 = lynch
+#  3 = hboc
+#  4 = other
+#
+print2('Determining thematic disease group assignment....')
 subjectsDT['diseaseGroup'] = None
 
 # ~ 2d.i ~
-# Assign TDG based on one inclusion criteria
+# Assign TDG 2 or 3 based on one inclusion criteria. The following conditions
+# must be met.
+#   3: If there isn't a value and the value equals the 1st inclusion criteria value
+#   2: If there isn't a value and the value equals the 2nd inclusion criteria value
+#
 print2('Setting diseaseGroup based on `InclCriteria`....')
 
 subjectsDT['diseaseGroup'] = dt.Frame([
-  # TDG #3
-  # If there isn't a value and the value equals the 1st inclusion criteria value
-  diseaseGroups[f.id=='3','groupName'].to_list()[0][0]
-  if not bool(value) & (value == inclusionCriteria[f.id==1,'label'].to_list()[0][0])
-  else (
-    # FOR TDG #2
-    # If there isn't a value and the value equals the 2nd inclusion criteria value
-    diseaseGroups[f.id=='2','groupName'].to_list()[0][0]
-    if not bool(value) & (value == inclusionCriteria[f.id==2,'label'].to_list()[0][0])
-    else value
+  '3' if not bool(value) & (value == 1) else (
+    '2' if not bool(value) & (value == 2) else value
   )
   for value in subjectsDT['InclCriteria'].to_list()[0]
 ])
+
+# subjectsDT[:, dt.count(), dt.by(f.diseaseGroup)]
 
 # ~ 2d.ii ~
 # Assign TDG based on one inclusion criteria + gen
 # Does the case meet the following conditions?
 #
 #  [] The value "Other..." for inclusion criteria
-#  [] Has a classification of "Likely Pathogenic" or "Pathogenic"
+#  [] Has a classification of "Likely Pathogenic" (2) or "Pathogenic" (3)
 #  [] The gene appears in the disease group gene list
 #  [] A TDG was assigned not in an earlier step
 #
 print2('Setting diseaseGroup based on `InclCriteria`, classification, and gene...')
-genesByGroup=diseaseGroupCriteria[f.type=='GENE',(f.groupName,f.value)]
+genesByGroup=diseaseGroupCriteria[f.type=='GENE',(f.groupID,f.value)]
 geneList = genesByGroup['value'].to_list()[0]
 
 subjectsDT['diseaseGroup'] = dt.Frame([
   row[3] if not all(row) else (
-    genesByGroup[f.value==row[2], 'groupName'].to_list()[0][0]
+    genesByGroup[f.value==row[2], 'groupID'].to_list()[0][0]
     if (
-      row[0] == inclusionCriteria[f.id==3,'label'].to_list()[0][0] &
-      row[1] in ['Likely pathogenic', 'Pathogenic'] &
-      row[2] in geneList
+      # inclusion criteria == "Other proven pathogenic..."
+      (str(row[0]) == '3') &
+      (str(row[1]) == '2' or str(row[1]) == '3') &
+      (str(row[2]) in geneList)
     )
     else row[3]
-  ) 
-  
+  )
   for row in subjectsDT[:, (
     f.InclCriteria,
     f.VariantClass_1,
@@ -441,13 +500,12 @@ subjectsDT['diseaseGroup'] = dt.Frame([
 #  [] A TDG was assigned not in an earlier step
 #
 print2('Setting disease group based on ORDO codes....')
-
-ordoByGroup = diseaseGroupCriteria[f.type=='ORDO', (f.groupName,f.value)]
+ordoByGroup = diseaseGroupCriteria[f.type=='ORDO', (f.groupID,f.value)]
 ordoList = ordoByGroup['value'].to_list()[0]
 
 subjectsDT['diseaseGroup'] = dt.Frame([
   row[1] if bool(row[1]) else (
-    ordoByGroup[f.value==row[0], 'groupName'].to_list()[0][0]
+    ordoByGroup[f.value==row[0], 'groupID'].to_list()[0][0]
     if row[0] in ordoList
     else row[1]
   )
@@ -459,99 +517,122 @@ subjectsDT['diseaseGroup'] = dt.Frame([
 print2('Setting disease groups based on `InclCriteriaUnexplained`...')
 
 # set TDG #2 for unexplained criteria ID==2|3
-subjectsDT['diseaseGroup'] = dt.Frame(
+subjectsDT['diseaseGroup'] = dt.Frame([
   row[1] if bool(row[1]) else (
-    row[1] if not bool(row[0]) else (
-      diseaseGroups[f.id=='2','groupName'].to_list()[0][0]
-      if row[0] in inclusionCriteriaUnexplained[
-        (f.id==2) | (f.id==3),
-        f.label
-      ].to_list()[0]
-      else row[1]
-    )
+    '2'
+    if (str(row[0]) == '2') or (str(row[0] == '3'))
+    else row[1]
   )
   for row in subjectsDT[:, (f.InclCriteriaUnexplained, f.diseaseGroup)].to_tuples()
-)
+])
 
 # set TDG #3 for unexplained criteria ID==4|5
-subjectsDT['diseaseGroup'] = dt.Frame(
+subjectsDT['diseaseGroup'] = dt.Frame([
   row[1] if bool(row[1]) else (
-    row[1] if not bool(row[0]) else (
-      diseaseGroups[f.id=='2','groupName'].to_list()[0][0]
-      if row[0] in inclusionCriteriaUnexplained[
-        (f.id==4) | (f.id==5),
-        f.label
-      ].to_list()[0]
-      else row[1]
-    )
+    '2' if ((str(row[0]) == '4') or (str(row[0]) == '5'))
+    else row[1]
   )
   for row in subjectsDT[:, (f.InclCriteriaUnexplained, f.diseaseGroup)].to_tuples()
-)
+])
 
 # ~ 2d.v ~
 # Override TDG for special cases
 # If the group assignment is 2 and the ORDO code is 252202, then assign to group 4
 # ORDO Term: "Constitutional mismatch repair deficiency syndrome"
 print2('Overriding diseaseGroup based on group 2 assignment and ORDO code...')
-
 subjectsDT['diseaseGroup'] = dt.Frame([
-  diseaseGroups[f.id=='4','groupName'].to_list()[0][0]
-  if row[1] == '2' & row[0] == 'ORPHA:252202'
-  else row[1]
-  for row in subjectsDT[:, (f.ORDO, f.diseaseGroup)].to_tuple()
+  '4' if (row[1] == '2') & (str(row[0]) == 'ORPHA:252202') else row[1]
+  for row in subjectsDT[:, (f.ORDO, f.diseaseGroup)].to_tuples()
 ])
 
-# TODO
+
+# ~ 2d.vi-vii ~
+# Calculate rowsums and override
+# Find out which columns are present in the datasets
+geneCols = extractColumnNames(subjectsDT, re.compile(r'^(VariantGene_)'))
+classCols = extractColumnNames(subjectsDT, re.compile(r'^(VariantClass_)'))
+zygosityCols = extractColumnNames(subjectsDT, re.compile(r'^(VariantZygosity_)'))
+
 # ~ 2d.vi ~
-# Override TDG based on ....
-#
+# Override TDG based on gene and classification status
+calcRowSums(
+  data = subjectsDT,
+  colname='sumGeneClass',
+  genes = ['MLH1','MSH2', 'MSH6', 'PMS2'],
+  geneColumns = geneCols,
+  classColumns = classCols
+)
 
-# TODO Create a function that calculates row sums before
-subjectsDT[:, [('VariantGene' | 'VariantClass') in column for column in subjectsDT.names]]
-
-
+# Change the group to 4 if the sum >= 2 and the current group is 2
 subjectsDT['diseaseGroup'] = dt.Frame([
-  diseaseGroups[f.id=='4', 'groupName'].to_list()[0][0]
-  if (
-    value == diseaseGroups[f.id=='2', 'groupName'].to_list()[0][0]
-  ) else value
-  for value in subjectsDT['diseaseGroup'].to_list()[0]
+  '4' if (row[0] == '2') & (row[1] >= 2) else row[0]
+  for row in subjectsDT[:, ['diseaseGroup', 'sumGeneClass']].to_tuples()
 ])
 
-# TODO
-# SEE .R SCRIPT
+# ~ 2d.vii ~
+# Override TDG based on gene, classification, and zygosity
+calcRowSums(
+  data = subjectsDT,
+  colname='sumGeneClassZygosity',
+  genes = ['MLH1','MSH2', 'MSH6', 'PMS2'],
+  geneColumns = geneCols,
+  classColumns = classCols,
+  zygosityColumns=zygosityCols
+)
 
+# change the group to 4 if the sum >= 1 and the current group is 2
+subjectsDT['diseaseGroup'] = dt.Frame([
+  '4' if (row[0] == '2') & (row[1] >= 1) else row[0]
+  for row in subjectsDT[:, ['diseaseGroup', 'sumGeneClassZygosity']].to_tuples()
+])
 
 # ~ 2d.vii ~
 # Override TDG 3 based on ORDO code
-# Change to Group 4 if the following conditions are met
-#
-#   [ ] Current TDG is 3
-#   [ ] ORDO term is: Ataxia-telangiectasia
-#
-# TODO: Replace with new code block where ATM is checked
+calcRowSums(
+  data = subjectsDT,
+  colname = 'rowSumGroup3',
+  genes = ['ATM'],
+  geneColumns = geneCols,
+  classColumns = classCols
+)
 
-# subjectsDT['diseaseGroup'] = dt.Frame([
-#   row[1] if not bool(row[1]) else (
-#     diseaseGroups[f.id=='4', 'groupName'].to_list()[0][0]
-#     if (
-#       row[0] == 'ORPHA:100' &
-#       row[1] == diseaseGroups[f.id=='3', 'groupName'].to_list()[0][0]
-#     )
-#     else row[1]
-#   )
-#   for row in subjectsDT[:, (f.ORDO, f.diseaseGroup)].to_tuple()
-# ])
+# if rowsum > 1 and group is 3, change to group 4
+subjectsDT['diseaseGroup'] = dt.Frame([
+  '4' if (row[0] == '3') & (row[1] >= 1) else row[0]
+  for row in subjectsDT[:, ['diseaseGroup', 'rowSumGroup3']].to_tuples()
+])
 
 # ~ 2d.ix ~
 # Override TDG if ORDO is "Birt-Hogg-Dubé syndrome" (ORPHA:122)
 subjectsDT['diseaseGroup'] = dt.Frame([
-  diseaseGroups[f.id=='4', 'groupName'].to_list()[0][0]
-  if row[0] == 'ORPHA:122'
-  else row[1]
-  for row in subjectsDT[:, (f.ORDO, f.diseaseGroup)].to_tuple()
+  '4' if row[0] == 'ORPHA:122' else row[1]
+  for row in subjectsDT[:, (f.ORDO, f.diseaseGroup)].to_tuples()
 ])
 
+# ~ 2d.x ~
+# Summarise by group
+print2('Summarizing by groups....')
+countByGroup = subjectsDT[:, dt.count(), dt.by(f.diseaseGroup)]
+countByGroup['groupName'] = dt.Frame([
+  diseaseGroups[f.groupID == value, 'groupName'].to_list()[0][0]
+  for value in countByGroup['diseaseGroup'].to_list()[0]
+])
+
+# set undetermined group
+if countByGroup[f.diseaseGroup==None,:].nrows > 0:
+  countByGroup[f.diseaseGroup==None, 'groupName'] = 'Undetermined'
+
+# update ernstats datasets
+for value in countByGroup['groupName'].to_list()[0]:
+  if 'Other rare' in value:
+    group2 = ernstats[f.component=='table-enrollment-disease-group','label'].to_list()[0][2]
+    ernstats[f.label==group2,'value'] = countByGroup[f.groupName==value, 'count'].to_list()[0][0]
+  elif value in ernstats[f.component=='table-enrollment-disease-group','label'].to_list()[0]:
+    ernstats[f.label==value,'value'] = countByGroup[
+      f.groupName==value,'count'
+    ].to_list()[0][0]
+  else:
+    raise SystemError(f"Error: value '{value}' uknown. Please check disease group mappings....")
 
 #///////////////////////////////////////////////////////////////////////////////
 
