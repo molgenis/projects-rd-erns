ad65cad0585a9c8cee85afa881033ac4ae155af2
dcruvolo <davidruvolo51@gmail.com>
2023-08-16 10:36:23 +0200

    fix: added table overrides and auto increment keys

--- erns/cranio/model_convert.py
+++ erns/cranio/model_convert.py
@@ -2,9 +2,9 @@
 # FILE: model_convert.py
 # AUTHOR: David Ruvolo
 # CREATED: 2023-08-11
-# MODIFIED: 2023-08-11
+# MODIFIED: 2023-08-16
 # PURPOSE: convert EMX1 model to EMX2 format
-# STATUS: in.progress
+# STATUS: stable
 # PACKAGES: NA
 # COMMENTS: NA
 #///////////////////////////////////////////////////////////////////////////////
@@ -184,10 +184,11 @@ newOntologies = []
 for table in dt.unique(emx2['tableName']).to_list()[0]:
   
   # find pseudo-ontologies based on attribute names
-  if emx2[f.tableName==table,'columnName'].to_list()[0] == [None, 'id','label']:
+  columnNames = emx2[f.tableName==table,'columnName'].to_list()[0]
+  if (columnNames == [None, 'id','label']) or (columnNames == [None, 'id','value']):
     
     # set flag to remove old ref tables and log
-    emx2[(f.tableName==table) & (dt.re.match(f.columnName, 'id|label')), 'remove'] = True
+    emx2[(f.tableName==table) & (dt.re.match(f.columnName, 'id|label|value')), 'remove'] = True
     if table not in newOntologies:
       newOntologies.append(table)
     
@@ -209,6 +210,20 @@ del emx2['remove']
 for column in emx2.names:
   if (emx2[column].type == dt.Type.bool8):
     emx2[column] = emx2[:, as_type(f[column], dt.Type.str32)]
+    
+    
+# auto-increment keys
+for table in dt.unique(emx2['tableName']).to_list()[0]:
+  if emx2[(f.tableName==table) & (f.key!=None),'key'].nrows > 1:
+    start = 0
+    end = emx2[(f.tableName == table) & (f.key != None),'key'].nrows
+    seq = [value + 1 for value in list(range(start, end, 1))]
+    emx2[(f.tableName == table) & (f.key != None), 'key'] = dt.Frame(seq)
+    
+    
+# custom overrides
+emx2[(f.tableName=='hpo') & (f.columnName=='treeview'), 'columnType'] = 'hyperlink_array'
+emx2[(f.tableName=='subject') & (f.columnName=='HCP'), 'columnType'] = 'ref_array'
 
 
 emx2.to_csv('./erns/cranio/ern_cranio_molgenis.csv')
@@ -220,7 +235,7 @@ emx2.to_csv('./erns/cranio/ern_cranio_molgenis.csv')
 # Parse other sheets and save to file
 
 wb = pd.ExcelFile(file)
-wb.sheet_names
+# wb.sheet_names
 
 tablesToIgnore = [
   'packages',
@@ -230,10 +245,11 @@ tablesToIgnore = [
   'cranio_visits_cleft',
   'cranio_visits_synostosis',
   'cranio_variant',
-  'cranio_person',
   'cranio_family',
   'cranio_fammember'
 ]
+
+countries = wb.parse(sheet_name='cranio_country').rename(columns={'id':'country'})
 
 for sheet in tqdm(wb.sheet_names):
   if sheet not in tablesToIgnore:
@@ -241,8 +257,69 @@ for sheet in tqdm(wb.sheet_names):
     data = wb.parse(sheet_name=sheet)
     
     if table in newOntologies:
-      data = data.rename(columns={'label': 'name'}) \
-        .drop('id', axis=1)
+      if 'label' in list(data.columns):
+        data = data.rename(columns={'label': 'name'}).drop('id', axis=1)
+      if 'value' in list(data.columns):
+        data = data.rename(columns={'value': 'name'}).drop('id', axis=1)
+        
+    #///////////////////////////////////////
+    
+    # custom overrides: biobank
+    #   - fix: make sure all values start with http, remove values with slash
+    #   - fix: merge country names to align with ontology table
+    
+    if table == 'biobank':
+      data['url'] = data['url'].replace({
+        '/': None,
+        'www.latvianbiobank.lv': 'http://www.latvianbiobank.lv',
+        'www.oncolifes.nl': 'http://www.oncolifes.nl',
+        'www.lifelines.nl': 'http://www.lifelines.nl',
+        'www.nesda.nl': 'http://www.nesda.nl'
+      })
+
+      data = pd.merge(data, countries, on='country') \
+        .drop('country', axis=1) \
+        .rename(columns={'value': 'country'})
+    
+    # custom overrides: organisation
+    #   - fix: merge expertise names
+    #   - fix: merge country names
+    #   - temp fix: drop persons so import can pass
+    if table == 'organisation':
+      expertise = wb.parse(sheet_name='cranio_expertise').rename(columns={'id': 'expertise'})
+      data = pd.merge(data, expertise, on='expertise') \
+        .drop('expertise', axis=1) \
+        .rename(columns = {'value': 'expertise'})
+      
+      data = pd.merge(data, countries, on='country') \
+        .drop('country', axis=1) \
+        .rename(columns={'value': 'country'})
+      
+      data['principal_investigators'] = None
+      
+      
+    # custom override: tissueTyp
+    #   - fix: recode tissueTypes
+    if table == 'extra_gene':
+      tissueTypes = wb.parse(sheet_name='cranio_tissueType') \
+          .rename(columns={'id': 'tissueType'}) \
+          .set_index('tissueType')['label'] \
+          .to_dict()
+      
+      data['tissueType'] = data['tissueType'] \
+        .apply(lambda value: ','.join([
+            tissueTypes[val] for val in str(value).split(',')
+          ]) if str(value) != 'nan' else None
+        )
+      
+    
+    # custom override: person
+    if table == 'person':
+      data = pd.merge(data, countries, on='country') \
+        .drop('country', axis=1) \
+        .rename(columns={'value': 'country'})
+      
+    #///////////////////////////////////////
     
     data.to_csv(
       f'erns/cranio/lookups/{table}.csv',
