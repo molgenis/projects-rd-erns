c22abc487f80e22bd33dade7d2ed634006b1ba3a
dcruvolo <davidruvolo51@gmail.com>
2024-02-09 15:47:01 +0100

    adjusted file paths

--- erns/cranio/cranio_prep_organisations.py
+++ erns/cranio/cranio_prep_organisations.py
@@ -4,10 +4,7 @@ import json
 import pandas as pd
 
 if __name__ == '__main__':
-    data = pd.read_excel(
-        'erns/cranio/cranio_emx2.xlsx',
-        sheet_name='organisations'
-    )
+    data = pd.read_csv('erns/cranio/imports/organisations.csv')
 
     with open('erns/cranio/cranio_organisations.json', 'w', encoding='utf-8') as file:
         data_selection = data[['name', 'schemaName']].to_dict('records')
