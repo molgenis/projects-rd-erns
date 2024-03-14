778cea866741e67d1a9585a98b4fe990a93bc4e3
dcruvolo <davidruvolo51@gmail.com>
2024-03-14 11:20:47 +0100

    updated organisations list

---
+++ erns/skin/import_data.py
@@ -0,0 +1,14 @@
+"""Import data into emx2 instance"""
+
+from molgenis_emx2_pyclient import Client
+import pandas as pd
+
+WB = "./erns/skin/skin_emx2.xlsx"
+TOKEN = ""
+
+# import data from sheets
+organisations_df = pd.read_excel(WB, sheet_name="organisations",)
+organisations = organisations_df.to_dict('records')
+
+with Client("https://beta-ernskin.molgeniscloud.org", schema="ErnStats", token=TOKEN) as client:
+    client.save_schema(table="Organisations", data=organisations)
